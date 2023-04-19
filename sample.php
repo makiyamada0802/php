1f4b4aaef4e430af77a66d32d7db38d470126f23
maki <maki080218@gmail.com>
2023-04-19 13:47:09 +0000

    php1

---
+++ sample.php
@@ -0,0 +1,31 @@
+<?php
+// 変数　$a に　３　を代入
+$a = 3;
+// 変数　$b に　７　を代入
+$b = 7;
+// 変数 $a を出力
+echo $a + $b;
+// => 3と表示される
+$array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+$hello="Hello";
+$name="yamada";
+$world="'s WWorld!";
+echo$hello.$name.$world;
+$tech_boost='tech';
+$tech_boost.='_boost';
+echo$tech_boost;
+$calendar_2018 =[
+    "January"=>"1月",
+    "February"=>"2月",
+    "March"=>"3月",
+    "April"=>"4月",
+    "May"=>"5月",
+    "June"=>"6月",
+    "July"=>"7月",
+    "August"=>"8月",
+    "September"=>"9月",
+    "October"=>"10月",
+    "November"=>"11月",
+    "December"=>"12月",];
+    echo$calendar_2018["December"];
